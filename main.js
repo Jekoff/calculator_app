fe11f8866a32b5430e3eef96316bfc80b4314132
Jérôme Lanloze <99019668+Jekoff@users.noreply.github.com>
2023-07-23 04:53:20 +0200

    added operator key function

--- main.js
+++ main.js
@@ -34,3 +34,42 @@ if (action === 'clear') {
 if (action === 'calculate') {
     console.log('equal key!')
 }
+
+const display = document.querySelector('.calculator__display')
+
+keys.addEventListener('click', e => {
+  if (e.target.matches('button')) {
+    const key = e.target
+    const action = key.dataset.action
+    const keyContent = key.textContent
+    const displayedNum = display.textContent
+    // ...
+  }
+})
+
+if (!action) {
+    if (displayedNum === '0') {
+      display.textContent = keyContent
+    }
+  }
+
+  if (!action) {
+    if (displayedNum === '0') {
+      display.textContent = keyContent
+    } else {
+      display.textContent = displayedNum + keyContent
+    }
+  }
+
+  if (action === 'decimal') {
+    display.textContent = displayedNum + '.'
+  }
+
+  if (
+    action === 'add' ||
+    action === 'subtract' ||
+    action === 'multiply' ||
+    action === 'divide'
+  ) {
+    key.classList.add('is-depressed')
+  }
