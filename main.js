96a52e0c714044bf471ec8545c08e8c149632cc9
Jekoff <jlanloze@gmail.com>
2023-09-29 15:35:46 +0200

    commit

--- main.js
+++ main.js
@@ -138,3 +138,19 @@ if (action === 'calculator') {
 
   display.textContent = calculator(firstValue, operator, secondValue)
 }
+
+const calculate = (n1, operator, n2) => {
+  let result = ''
+
+  if (operator === 'add') {
+    result = parseFloat(n1) + parseFloat(n2)
+  } else if (operator === 'subtract') {
+    result = parseFloat(n1) - parseFloat(n2)
+  } else if (operator === 'multiply') {
+    result = parseFloat(n1) * parseFloat(n2)
+  } else if (operator === 'divide') {
+    result = parseFloat(n1) / parseFloat(n2)
+  }
+
+  return result
+}
